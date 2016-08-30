fcdf71e199939f14d1950b4656f5ce1b79f27301
DELL\quyenafc <trongquyenafc@gmail.com>
2016-08-30 11:40:39 -0700

    this add class model

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class Auditable : IAuditable
+    {
+        [MaxLength(256)]
+        public string CreateBy{ set; get; }
+
+        public DateTime? CreateDate { set; get; }
+
+        [MaxLength(256)]
+        public string UpdateBy { set; get; }
+     
+
+        public DateTime? UpdateDate { set; get; }
+     
+
+
+        [MaxLength(256)]
+        public string MetaDescription { set; get; }
+        [MaxLength(256)]
+        public string Metakeyword { set; get; }
+      
+
+        public bool Status { set; get; }
+       
+    }
+}
---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+
+
+        DateTime? CreateDate { set; get; }
+        string CreateBy { set; get; }
+        DateTime? UpdateDate { set; get; }
+        string UpdateBy { set; get; }
+        string Metakeyword { set; get; }
+        string MetaDescription { set; get; }
+        bool Status { set; get; }
+    }
+}
