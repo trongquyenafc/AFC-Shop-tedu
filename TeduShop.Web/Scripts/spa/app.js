2aafdacf0c4b3892b99616bb43a0b5c299894810
DELL\quyenafc <trongquyenafc@gmail.com>
2016-09-03 10:54:33 -0700

    this is scope and rootscope

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var myapp = angular.module('mymodel', []);
+myapp.controller('schoolcontroller', schoolcontroller);
+myapp.controller('studentmycontroller', studentmycontroller);
+myapp.controller('techmycontroller', techmycontroller);
+
+
+function schoolcontroller($scope) {
+
+    $scope.message = "annomount from school.";
+}
+
+
+function studentmycontroller($scope) {
+      $scope.message = "this is message angular from student" //neu khong co message thi no an theo message shool
+}
+
+
+function techmycontroller($scope) {
+    $scope.message = "this is message angular from teacher"//neu khong co message thi no an theo message shool
+}
+
+
+
+//function studentmycontroller($rootScope,$scope) {
+//    $rootScope.message = "this is message angular from student"
+//    //bien rootScope la bien toan cuc neu co rootSocpe thi cac bien cung nhau no se hien thi ra giong nhau
+//}
